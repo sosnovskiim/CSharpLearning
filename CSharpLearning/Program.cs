ff42e492cce70167f55cd970f3d5e14bdd0f48d9
sosnovskiima <mikhailsosnovskii2805@gmail.com>
2024-10-15 12:44:07 +0400

    5

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Task7.Execute();
+            Task8.Execute();
         }
     }
 }
---
+++ CSharpLearning/Task8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharpLearning
+{
+    /*
+     * Дана строка, в которой содержится осмысленное текстовое сообщение.
+     * Слова сообщения разделяются пробелами и знаками препинания.
+     * Вывести только те слова сообщения, которые содержат не более чем n букв.
+     */
+    internal class Task8
+    {
+        internal static void Execute()
+        {
+            Console.WriteLine("Введите текстовое сообщение:");
+            string s = Console.ReadLine();
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            char[] separators = { ' ', '\r', '\t', '\n', ',', '.', ';', ':', '!', '?', '—', '–', '\'', '\"' };
+            string[] words = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Слова, содержащие не более n букв:");
+            foreach (string word in words)
+            {
+                if (word.Length <= n)
+                {
+                    Console.Write("{0} ", word);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
